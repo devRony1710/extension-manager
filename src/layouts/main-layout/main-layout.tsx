bc3f263c331aa159964ecc09ae8f25e22298610b
Rony Antolinez <rony1710.dev@gmail.com>
2025-05-21 12:56:00 -0500

    feat: add icons

--- src/layouts/main-layout/main-layout.tsx
+++ src/layouts/main-layout/main-layout.tsx
@@ -1,12 +1,17 @@
 import { FC } from "react";
 import { MainLayoutProps } from "./main-layout.types";
 import styles from "./main-layout.module.css";
+import LogoWhite from "@/assets/icons/logo-white.svg";
 
 export const MainLayout: FC<MainLayoutProps> = ({ children }) => {
   return (
     <section className={styles["main-wrapper"]}>
-      <span>Hola mundo</span>
-      {children}
+      <section className={styles["content-wrapper"]}>
+        <div className={styles["header-wrapper"]}>
+          <img src={LogoWhite} alt="extension manager logo" />
+        </div>
+        {children}
+      </section>
     </section>
   );
 };
