49cc9a2e370f4f88fbb3aefc94d0573eb470d67d
Rony Antolinez <rony1710.dev@gmail.com>
2025-07-01 22:34:17 -0500

    wip: add simulate request for an extension list api

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -2,18 +2,60 @@ import { Filters } from "@/components/filters/filters";
 import styles from "./home.module.css";
 import { FILTERS_OPTIONS } from "@/lib/constants/general-constants";
 import { OptionType } from "@/components/filters/filters.types";
+import { useQuery } from "@tanstack/react-query";
+import { GET_EXTENSIONS_LIST } from "@/lib/constants/request-keys";
+import {
+  ExtensionListDataResponse,
+  getExtensionsList,
+} from "@/api/get/get-extensions-list";
+import { useEffect, useState } from "react";
 
 export const Home = () => {
+  const [filterSelected, setFilterSelected] = useState<string | null>("all");
+  const [extensionsData, setExtensionsData] = useState<
+    ExtensionListDataResponse[]
+  >([]);
+
+  const { data } = useQuery({
+    queryKey: [GET_EXTENSIONS_LIST],
+    queryFn: () => getExtensionsList(),
+  });
+
   // MOVER LUEGO
   const getActiveOption = (option: OptionType) => {
-    console.log(option);
+    setFilterSelected(option.value);
   };
+
+  useEffect(() => {
+    if (filterSelected === "all") {
+      setExtensionsData(data);
+    }
+
+    if (filterSelected === "active") {
+      setExtensionsData(data.filter((item) => item.isActive));
+    }
+
+    if (filterSelected === "inactive") {
+      setExtensionsData(data.filter((item) => !item.isActive));
+    }
+  }, [filterSelected, data]);
 
   return (
     <section className={styles["homeMainContainer"]}>
       <h1 className={styles["homeTitle"]}>Extension List</h1>
 
       <Filters options={FILTERS_OPTIONS} getActiveOption={getActiveOption} />
+
+      <section className={styles["extensionsContainer"]}>
+        {extensionsData.map((extension) => (
+          <div key={extension.name} className={styles["extensionItem"]}>
+            <img src={extension.logo} alt={extension.name} />
+            <h2>{extension.name}</h2>
+            <p>{extension.description}</p>
+            <button>Install</button>
+          </div>
+        ))}
+      </section>
     </section>
   );
 };
