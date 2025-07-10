a216c16e61a31e1fda9cdabca09f8beca993bb1d
Rony Antolinez <rony1710.dev@gmail.com>
2025-07-10 00:47:16 -0500

    feat: remove files generated by build

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import { MainLayout } from "@/layouts";
-import { Home } from "@/pages";
-function App() {
-    return (_jsx(MainLayout, { children: _jsx(Home, {}) }));
-}
-export default App;
--- src/hooks/use-theme-context.js
+++
@@ -1,7 +0,0 @@
-import { useContext } from "react";
-import { AppContext } from "@/context/app-context";
-export const useThemeContext = () => {
-    const { theme, changeTheme } = useContext(AppContext);
-    const isDarkTheme = theme === "dark";
-    return { theme, changeTheme, isDarkTheme };
-};
